c66a94c0873aa8ea2f08012ae35b54c7103ca678
Jose Abella <abellajose9@gmail.com>
2023-10-15 11:37:24 -0500

    Examen js-III

---
+++ script.js
@@ -0,0 +1,37 @@
+function datos() {
+
+    let formulario = document.getElementById("formulario").value;
+    let nombre = document.getElementById("nombre").value;
+
+
+
+    let p1 = document.createElement("p");
+    let p1Texto = document.createTextNode(nombre);
+    p1.appendChild(p1Texto);
+
+
+    datos.appendChild(p1);
+
+
+    let miArreglo = ["Nombre"];
+    miArreglo.push("Pais").innerHTML
+    document.getElementById("ver");
+
+    miArreglo.unshift("Nacionalidad").innerHTML
+    document.getElementById("ver");
+
+    miArreglo.pop("Barrio").innerHTML
+    document.getElementById("ver");
+
+    miArreglo.shift("").innerHTML
+
+    miArreglo.length("ver").innerHTML
+    document.getElementById("ver");
+
+    document.getElementById("ver")
+
+
+
+
+
+}
